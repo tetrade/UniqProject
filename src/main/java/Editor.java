6f19b01ea3495d98d164c4b462b0469f3c2e68a3
tetrade <piatlin2002@gmail.com>
2021-02-07 22:53:55 +0300

    adding

---
+++ src/main/java/Editor.java
@@ -0,0 +1,13 @@
+public class Editor {
+//    private String comLine;
+    private boolean iF;
+    private boolean uF;
+    private boolean cF;
+    private int sF;
+    private int oF;
+    private String file;
+
+    public Editor(String comLine) {
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String comLine = in.nextLine();
+        System.out.println(Arrays.toString(comLine.split("\\s-[ioucs]")));
+    }
+}
